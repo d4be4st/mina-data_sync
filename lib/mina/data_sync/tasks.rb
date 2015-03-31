ab1837daa210f70c33ee58bad327753c67359d47
Stjepan Hadjic <d4be4st@gmail.com>
2015-03-31 10:45:39 +0200

    removed --clean for -a

--- lib/mina/data_sync/tasks.rb
+++ lib/mina/data_sync/tasks.rb
@@ -7,7 +7,7 @@ namespace :data_sync do
     queue "#{DATABASE_CONF}"
     queue "cd #{deploy_to}/#{current_path}"
     queue "ARGS=$(database_conf #{database_path} #{rails_env})"
-    queue! "pg_dump $ARGS #{pg_options} --clean > #{backup_file}"
+    queue! "pg_dump $ARGS #{pg_options} > #{backup_file}"
 
     to :after_hook do
       queue "echo '-----> Copying backup'"
@@ -15,7 +15,7 @@ namespace :data_sync do
       queue "ARGS=$(database_conf #{database_path} development)"
       queue! "scp -P #{port} #{user}@#{domain}:#{deploy_to}/#{current_path}/#{backup_file} #{backup_path}/#{backup_file}"
       queue "echo '-----> Restoring database'"
-      queue! "pg_restore $ARGS #{pg_options} --clean #{backup_path}/#{backup_file}"
+      queue! "pg_restore $ARGS #{pg_options} #{backup_path}/#{backup_file}"
     end
   end
 
@@ -25,7 +25,7 @@ namespace :data_sync do
       queue "#{DATABASE_CONF}"
       queue "echo '-----> Dumping database'"
       queue "ARGS=$(database_conf #{database_path} development)"
-      queue! "pg_dump $ARGS #{pg_options} --clean > #{backup_file}"
+      queue! "pg_dump $ARGS #{pg_options} > #{backup_file}"
       queue "echo '-----> Copying backup'"
       queue! "scp -P #{port} #{backup_file} #{user}@#{domain}:#{deploy_to}/#{current_path}/#{backup_file}"
     end
@@ -34,6 +34,6 @@ namespace :data_sync do
     queue "#{DATABASE_CONF}"
     queue "cd #{deploy_to}/#{current_path}"
     queue "ARGS=$(database_conf #{database_path} #{rails_env})"
-    queue! "pg_restore $ARGS #{pg_options} --clean #{backup_file}"
+    queue! "pg_restore $ARGS #{pg_options} #{backup_file}"
   end
 end
