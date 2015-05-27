1956f881e2153731390b6723fb8c9f55bb9d8aac
Stjepan Hadjic <d4be4st@gmail.com>
2015-05-27 15:27:45 +0200

    bugfix

--- lib/mina/data_sync/defaults.rb
+++ lib/mina/data_sync/defaults.rb
@@ -1,4 +1,4 @@
 set_default :database_path, "config/database.yml"
 set_default :remote_backup_path, 'tmp'
-set_default :local_backup_path, { ENV['DATA_SYNC_BACKUP_PATH'] || 'tmp/backups' }
+set_default :local_backup_path, -> { ENV['DATA_SYNC_BACKUP_PATH'] || 'tmp' }
 set_default :restore_data, -> { ENV['restore'] || 'true' }
--- lib/mina/data_sync/version.rb
+++ lib/mina/data_sync/version.rb
@@ -1,5 +1,5 @@
 module Mina
   module DataSync
-    VERSION = "0.0.9"
+    VERSION = "0.0.10"
   end
 end
