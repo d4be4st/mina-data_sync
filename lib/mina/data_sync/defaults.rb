0bfca496f2a8c4f32505f8a3430d5e38b26b2853
tkarpesh <tatiana.karpesh@gmail.com>
2016-06-07 00:00:02 +0300

    disallow_pushing option

--- lib/mina/data_sync/defaults.rb
+++ lib/mina/data_sync/defaults.rb
@@ -3,3 +3,4 @@ set_default :remote_backup_path, 'tmp'
 set_default :local_backup_path, -> { ENV['DATA_SYNC_BACKUP_PATH'] || 'tmp' }
 set_default :restore_data, -> { ENV['restore'] || 'true' }
 set_default :dump_data, -> { ENV['dump'] || 'true' }
+set_default :disallow_pushing, false
